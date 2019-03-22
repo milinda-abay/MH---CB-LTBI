44ebc054eeca08223d1b8954b7e4d74b3d44280b
James Trauer <james.trauer@monash.edu>
2019-03-22 21:55:28 +1100

    Add line of code to quickly reduce the number of cohorts that have to be tracked in model running
    Plus some unnecessary fiddling with syntax, comments and whitespace.

--- CB-TLTBI.R
+++ CB-TLTBI.R
@@ -28,7 +28,7 @@ source("CB-TLTBI functions.R")
 # This function uses the above three Fix* functions. 
 # Run once to create the *.rds objects (vic.fertility, vic.mortality, vic.migration)
 # based on ABS's population porjection data
-CreateRDSDataFiles()
+#CreateRDSDataFiles()
 
 
 # Read the data files (if required)
@@ -44,14 +44,14 @@ vic.pop <- readRDS("Data/vic.pop.rds")
 RRates <- readRDS("Data/RRates.rds") # this is also required
 vic.tb.mortality <- readRDS("Data/vic.tb.mortality.rds") # this is also required
 
-# Creatinga a vector of state names
+# Creating a vector of state names
 state.names <- c("p.sus", "p.sus.fp.t", "p.sus.fp.nt", "p.sus.fp.tc", "p.sus.tn",
                  "p.ltbi", "p.ltbi.tp.t", "p.ltbi.tp.tc", "p.ltbi.tp.tc.tb", "p.ltbi.tp.tc.tbr",
                  "p.ltbi.tp.nt", "p.ltbi.tp.nt.tb", "p.ltbi.tp.nt.tbr", "p.ltbi.fn", "p.ltbi.fn.tb",
                  "p.ltbi.fn.tbr", "p.ltbi.tb", "p.ltbi.tbr", "p.ltbi.tp.tc.tb.death", "p.ltbi.tp.nt.tb.death",
                  "p.ltbi.fn.tb.death", "p.ltbi.tb.death", "p.death")
 
- #CreateStates(state.names) # --- not used --- instantiates a set of states objects with default vaules
+#CreateStates(state.names) # --- not used --- instantiates a set of states objects with default vaules
 
 # Creates an unevaluated transition matrix
 # Use 'CMP' for complement and 'param$*' for parameters.
@@ -80,8 +80,6 @@ transMatrix4R <- DefineTransition(CMP, 0.05 * 0.07 * 0.6818, 0.05 * 0.07 * 0.318
                                   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0,
                                   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, state.names = state.names)
 
-
-
 # Baseline transition matrix
 transMatrixBaseline <- DefineTransition(CMP, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, param$MR,
                                         0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
@@ -108,11 +106,9 @@ transMatrixBaseline <- DefineTransition(CMP, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                                         0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
                                         state.names = state.names)
 
-
 # Depending on which model to run
 transMatrix <- transMatrix4R
 # transMatrix <- transMatrixBaseline
-
 
 
 # Creates an unevaluated set of parameters
@@ -121,25 +117,26 @@ parameters <- DefineParameters(MR = Get.MR(DT, year, rate.assumption = "High"),
                                TBMR = Get.TBMR(DT, year))
 
 
-
 # Uses aust.LGA.rds file to create a sample input
 pop.master <- CreatePopulationMaster()
 
 # Model parameters
 start.year <- 2016
-year <- start.year # initialise year with start.year
+year <- start.year # Initialise year with start.year
 markov.cycle <- 0 # Tracks the current cycle
-cycles <- 10 # Model run cycles 
+cycles <- 10 # Model run cycles
 
 # Creates and initialises the population output table for the model (markov.cycle = 0)
 # pop.output <- pop.master[YARP <= year & AGERP <= 40 & AGEP <= 40][, cycle := markov.cycle][1:100]
 pop.output <- pop.master[YARP <= year][, cycle := markov.cycle]
 
-
-
+# Toggle to reduce number of cohorts to evaluate to speed running time
+cohorts_to_track <- nrow(pop.output)
+#cohorts_to_track <- 1e4
+  
 # TODO - If start.year != 2016 then recalculate AGEP at start.year!
 
-pop.output <- RunModel(pop.output)
+pop.output <- RunModel(pop.output[1: cohorts_to_track])
 
 # Saves output
 saveRDS(pop.output, "Data/pop.output.Baseline.rds")
